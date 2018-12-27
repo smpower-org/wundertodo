4266fb6036c727adc437d32e38f8fe88730adcf9
smpower <rf.wangchn>
2018-12-27 22:12:50 +0800

    add main component

---
+++ src/main/views/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './main.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        Main Component...
+      </div>
+    );
+  }
+}
+
+export default Main;
