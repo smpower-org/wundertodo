b18de69da2d46bb3db83c04de77f0cf44db9af18
smpower <rf.wangchn>
2018-12-27 22:10:02 +0800

    add regist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {view as Login} from './login/';
+import {view as Regist} from './regist/';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Login />
+        <Regist />
       </div>
     );
   }
---
+++ src/regist/views/regist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './regist.css';
+
+class Regist extends React.Component {
+  render() {
+    return (
+      <div className="regist">
+        Regist Components...
+      </div>
+    );
+  }
+}
+
+export default Regist;
